06f83a83a672dc6b497adcfede375628e03b191f
Dominic Welsh <domwelsh@gmail.com>
2024-01-08 13:53:48 +0000

    Added type hints and doc strings to relevant functions

--- project.py
+++ project.py
@@ -79,7 +79,13 @@ def mode_2():
                     print(w)
 
 
-def ready_input():
+def ready_input() -> str:
+    """
+    Asks the user if they want to do the selected mode again
+
+    :return: Users input as a string. Anything except only 'q' will continue the practice
+    """
+
     r_input = input("Press Enter when ready to generate letters. "
                     "The '?' counts as any letter. "
                     "Enter 'q' to quit: ")
@@ -93,7 +99,14 @@ def reset_tiles():
     print("Resetting tiles...")
 
 
-def user_input_word(letters):
+def user_input_word(letters: str) -> str:
+    """
+    Asks the user to enter their guess for best word based on letters
+
+    :param letters: Available letters to make a word out of. ? represent any letter
+    :return: A string that is either "" or only has letters from `letters` param
+    """
+
     while True:
         guess = input("Enter your word (if no valid words, press Enter with no input): ")
         if is_valid(guess, letters):
@@ -102,7 +115,14 @@ def user_input_word(letters):
             print("Invalid word. Use only the provided letters.")
 
 
-def is_valid(word, available_letters):
+def is_valid(word: str, available_letters: str) -> bool:
+    """
+    Checks if the user's input is a valid word based on available letters
+
+    :param word: User's word they inputted
+    :param available_letters: Valid letters to use, ? means any letter
+    """
+
     blank_tiles = available_letters.count('?')
     for letter in word:
         if word.count(letter) > available_letters.count(letter):
@@ -114,12 +134,27 @@ def is_valid(word, available_letters):
     return True
 
 
-def no_points_generate_letters(amount=7):
+def no_points_generate_letters(amount: int = 7) -> str:
+    """
+    Generate a string of random letters, no points associated
+    ? represents blank tiles which can be used as any letter
+
+    :param amount: Number of letters to return
+    :return: A string of letters that are to be used to make a word
+    """
+
     alphabet = 'abcdefghijklmnopqrstuvwxyz?'
     return ''.join(random.sample(alphabet, amount))
 
 
-def longest_words(letters):
+def longest_words(letters: str) -> list | str:
+    """
+    Creates a list of the longest words based on available letters, or says if there are no valid words
+
+    :param letters: Available letters to make words from
+    :return: A list of longest words, or a string saying no words can be made from the letters
+    """
+    
     english_word_list = []
     for word in twl.anagram(letters):
         english_word_list.append(word)
